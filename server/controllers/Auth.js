65e423840517a1f4f8a68a2a61cd7871d280ba80
sumitsingh00 <sumit00134@gmail.com>
2024-04-05 02:58:55 +0530

    LastCommit

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -8,7 +8,7 @@ const { passwordUpdated } = require("../mail/templates/passwordUpdate")
 const Profile = require("../models/Profile")
 require("dotenv").config()
 
-// Signup Controller for Registering USers
+// Signup Controller for Registering USer
 
 exports.signup = async (req, res) => {
   try {
